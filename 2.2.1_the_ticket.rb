1057ce50dd47c31b0ec522218fb56d844729f957
Charlotte Fereday <cbrfereday@gmail.com>
2017-02-19 15:16:54 +0000

    Adds in first two exercises of the well grounded rubyist

---
+++ 2.2.1_the_ticket.rb
@@ -0,0 +1,77 @@
+ticket = Object.new
+
+def ticket.date
+  "01/02/03"
+end
+
+def ticket.venue
+  "Town Hall"
+end
+
+def ticket.event
+  "Author's reading"
+end
+
+def ticket.performer
+  "Mark Twain"
+end
+
+def ticket.seat
+  "Second Balcony, row J, seat 12"
+end
+
+def ticket.price
+  5.50
+end
+
+
+print "This ticket is for: "
+print ticket.event + ", at "
+print ticket.venue + ", on "
+print ticket.date + " ."
+print "The performer is "
+puts ticket.performer + " ."
+print "The seat is "
+print ticket.seat + ", "
+print "and it costs $"
+puts "%.2f." % ticket.price
+
+#knowledge necessary for the program to do anything useful resides in the object
+#the ticket object has the knowledge; you tap into that knowledge by asking the ticket for it, via method calls
+
+#string interpolation & concatenation
+
+puts "This ticket is for: #{ticket.event}, at #{ticket.venue}." +
+  "The performer is #{ticket.performer}." +
+  "The seat is #{ticket.seat}, " +
+  "and it cost $#{"%.2f" % ticket.price}"
+
+  #Expressing Boolean state in a method
+  def ticket.availability_status
+    "sold"
+  end
+
+  #use a boolean to give its availability status
+  def ticket.available?
+    false
+  end
+
+  #true.true & false are objects
+
+if ticket.available?
+  puts "You're in luck!"
+else
+  puts "Sorry--that seat has been sold."
+end
+
+#more to truth & falsehood than true & false objects
+#every  expression in ruby evaluates to an object- every object in ruby has a truth value
+#truth value in almost every is true
+#only objects false- boolean false & nil
+
+if puts "You'll see this"
+  puts "but not this"
+end
+
+#the first puts is executed but the value it returns (nil) isn't true in the Boolean sense, 2nd puts isn't executed
+#every object has a boolean value  
---
+++ ch1.rb
@@ -0,0 +1,39 @@
+#objects are handled via variables that represent them
+#ask whether a considers itself equals to b
+#engineering of your object plays a clear role & can peform actions related to that role
+#manipulating data through objects via program-language driven principle object orientation
+#perform calculations, data manipulation, input/output operations by creating objects & asking them to perform actions & you with information
+
+#object-orientated software a matter of figuring out what your objects to be: what they should do, how they'll interact with each other, how many there should be
+
+#classes bundle & label behaviours
+#every object has the potential to "learn" behaviors (methods) that its class didn't teach it
+
+def obj.talk
+  puts "I am an object."
+  puts "(Do you object?)"
+end
+
+#dot-based message-sending syntax
+#The dot (.) is the message sending operator. Message on the right is ent to the object (receiver) on the left
+#Receiver can be & often is represented by a variable that stands in for an obkect. Receiver can also be a literal object construct (e.g. a string in quotation marks)
+#the message being sent is almost always the same name of a method (e.g. talk). The object tries to act on the assumption that the message is the name of a method. If nomethod, error-handling measures taken
+
+
+#Arguments
+#Can be required or optional
+
+def obj.c2f(c)
+  c * 9.0 / 5 + 32
+end
+
+puts obj.c2f(100)
+
+
+#Return value method
+#Ruby made up of expressions, each of which evaluates to a particular value.
+#When you call a method, the method call evaluates to something
+#you have to use return when:
+#you return multiple values which will be wrapped in an array return a, b, c
+#have to use return if you want to return from somewhere in the middle of a method
+#whether you use return or not something will be returned from every method call 
